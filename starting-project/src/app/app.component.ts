c5c2309ca2afddc6573545670e27118879235f3b
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-03-31 19:26:38 -0700

    custom events

--- starting-project/src/app/app.component.ts
+++ starting-project/src/app/app.component.ts
@@ -12,4 +12,8 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  onSelectUser(userId: string) {
+    console.log('Selected user:', userId);
+  }
 }
