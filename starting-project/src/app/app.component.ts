901dd357b4ee392203e10ef4ea8d95455f70b006
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-04-01 21:14:59 -0700

    tasks component

--- starting-project/src/app/app.component.ts
+++ starting-project/src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(userId: string) {
-    console.log('Selected user:', userId);
+    this.selectedUserId = userId;
   }
 }
