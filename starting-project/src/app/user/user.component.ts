5225a09e8dcb33a44652223b5e9a9637b50fbac1
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-04-06 22:19:16 -0700

    using interface

--- starting-project/src/app/user/user.component.ts
+++ starting-project/src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// }
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,11 +20,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: {
-    id: string;
-    name: string;
-    avatar: string;
-  };
+  @Input({ required: true }) user!: User;
 
   @Output() select = new EventEmitter<string>();
 
